ffc3b856a700f953c55f421771f10ef43b39b90d
Ronny-01 <ronakmankale17@gmail.com>
2026-02-16 20:06:48 +0530

    Add files via upload

---
+++ objectliterals.js
@@ -0,0 +1,12 @@
+const learner = {
+    name: "Ronak",
+    qualification: "graduate",
+    current:"learning Javascript",
+    situation:"javascript is getting onto my nerves",
+    workingonmern: ["monday","tuesday","wednesday","thursday","friday","saturday","sunday","entire week"]
+}
+
+ console.log(learner.name);
+ console.log(learner.current);
+ console.log(learner.situation);
+ console.log(learner.workingonmern[7]);
